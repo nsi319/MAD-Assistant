d09062e44123fafa3453c88031c19918b3c37ce6
nsi319 <sairam.nareniyer@gmail.com>
2021-05-02 18:34:53 +0530

    Update: Question count, release apk

--- app/src/main/java/com/appdevlab/mad/ui/quiz/QuizFragment.java
+++ app/src/main/java/com/appdevlab/mad/ui/quiz/QuizFragment.java
@@ -34,7 +34,7 @@ import static android.view.View.VISIBLE;
 public class QuizFragment extends Fragment {
 
     int qno = 1;
-    int total = 7;
+    int total ;
     ArrayList<String> questions;
 
     Button back,next;
@@ -45,51 +45,66 @@ public class QuizFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view =  inflater.inflate(R.layout.fragment_quiz,container,false);
 
-        questions = new ArrayList<>();
-        for(int i=1;i<=total;i++)
-            questions.add(String.valueOf(i));
-        Collections.shuffle(questions);
-
-        View root  = view.findViewById(R.id.coordinator);
-
-        internetConnectivity = new InternetConnectivity();
-
-        next = view.findViewById(R.id.next);
-        back = view.findViewById(R.id.back);
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference databaseReference = database.getReference("quiz");
 
         LoadQuestionFragment loadQuestionFragment = new LoadQuestionFragment();
         FragmentTransaction fragmentTransaction = getChildFragmentManager().beginTransaction();
         fragmentTransaction.replace(R.id.frame,loadQuestionFragment);
         fragmentTransaction.commit();
 
-
-        if(internetConnectivity.isInternetAvailable())
-            addNewQuestion(qno);
-        else
-            Snackbar.make(root,"No Internet Connection", Snackbar.LENGTH_LONG).show();
-
-        next.setOnClickListener(new View.OnClickListener() {
+        databaseReference.child("total").get().addOnCompleteListener(new OnCompleteListener<DataSnapshot>() {
             @Override
-            public void onClick(View v) {
-                qno++;
-                if(internetConnectivity.isInternetAvailable())
-                    addNewQuestion(qno);
-                else
-                    Snackbar.make(root,"No Internet Connection", Snackbar.LENGTH_LONG).show();
+            public void onComplete(@NonNull Task<DataSnapshot> task) {
+                if (!task.isSuccessful()) {
+                    total = 0;
+                }
+                else {
+                    total = Integer.parseInt(task.getResult().getValue().toString());
+                    questions = new ArrayList<>();
+                    for(int i=1;i<=total;i++)
+                        questions.add(String.valueOf(i));
+                    Collections.shuffle(questions);
+
+                    View root  = view.findViewById(R.id.coordinator);
+
+                    internetConnectivity = new InternetConnectivity();
+
+                    next = view.findViewById(R.id.next);
+                    back = view.findViewById(R.id.back);
+
+
+                    if(internetConnectivity.isInternetAvailable())
+                        addNewQuestion(qno);
+                    else
+                        Snackbar.make(root,"No Internet Connection", Snackbar.LENGTH_LONG).show();
+
+                    next.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            qno++;
+                            if(internetConnectivity.isInternetAvailable())
+                                addNewQuestion(qno);
+                            else
+                                Snackbar.make(root,"No Internet Connection", Snackbar.LENGTH_LONG).show();
+                        }
+                    });
+
+                    back.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            qno--;
+
+                            if(internetConnectivity.isInternetAvailable())
+                                addNewQuestion(qno);
+                            else
+                                Snackbar.make(root,"No Internet Connection", Snackbar.LENGTH_LONG).show();
+                        }
+                    });
+                }
             }
         });
 
-        back.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                qno--;
-
-                if(internetConnectivity.isInternetAvailable())
-                    addNewQuestion(qno);
-                else
-                    Snackbar.make(root,"No Internet Connection", Snackbar.LENGTH_LONG).show();
-            }
-        });
 
         return view;
     }
@@ -117,6 +132,7 @@ public class QuizFragment extends Fragment {
                     Toast.makeText(getActivity(),"Fetched question " + qno + " successfully",Toast.LENGTH_SHORT).show();
                     String title = "QUESTION " + qno;
                     String value = String.valueOf(task.getResult().getValue());
+                    value = value.trim();
                     String question = value.split("!!ans!!")[0];
                     String []options = value.split("!!ans!!")[1].split(";");
 
